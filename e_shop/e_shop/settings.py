e9ad17c245627b4e13c7785926cdca7f31602450
Lexx Luey <71068965+LexxLuey@users.noreply.github.com>
2021-10-14 17:31:24 +0100

    Update settings.py
    Clear pwd

--- e_shop/e_shop/settings.py
+++ e_shop/e_shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c9t3&42zvl9699t+w=au+8mbw95#2j+@r)%4&&ylay(yn!m4e^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
